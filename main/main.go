994942fd97e424a1dc150cf2851a2efa2692fdea
Ben Abrams <tacodude12345@gmail.com>
2020-09-06 18:15:46 -0400

    changed sheet to character, added ping check, began health check

--- main/main.go
+++ main/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/geeksheik9/sheet-CRUD/config"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
 
 func main() {
-	fmt.Println("Hello github")
+	logrus.Info("INITIALIZING SHEET CRUD")
+
+	accessor := viper.New()
+
+	_, err := config.New(accessor)
+	if err != nil {
+		log.Fatalf("ERROR LOADING CONFIG: %v", err.Error())
+	}
 }
---
+++ pkg/handler/character.go
@@ -0,0 +1,51 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+//CharacterDatabase is the interface setup for accesssing the character database
+type CharacterDatabase interface {
+	GetCharacterSheets()
+	FindCharacterSheetByID()
+	FindCharacterSheetByName()
+	UpdateCharacterSheetByID()
+	InsertCharacterSheet()
+	DeleteCharacterSheetByID()
+	FindArchivedCharacterSheetByID()
+	FindArchivedCharacterSheetByName()
+	Ping() error
+}
+
+//CharacterService is the implementation of the service to access character sheets
+type CharacterService struct {
+	Version  string
+	Database CharacterDatabase
+}
+
+//Routes sets up the routes for the RESTful interface
+func (s *CharacterService) Routes(r *mux.Router) *mux.Router {
+	r.HandleFunc("/ping", s.PingCheck).Methods(http.MethodGet)
+	r.Handle("/health", s.healthCheck(s.Database)).Methods(http.MethodGet)
+
+	return r
+}
+
+//PingCheck checks that the app is running and returns 200, OK, version
+func (s *CharacterService) PingCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("OK, " + s.Version))
+}
+
+func (s *CharacterService) healthCheck(database CharacterDatabase) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		dbErr := database.Ping()
+
+		Response := model.HealthCheckResponse{
+			APIVersion: s.Version,
+			DBError:    dbErr.Error(),
+		}
+	})
+}
